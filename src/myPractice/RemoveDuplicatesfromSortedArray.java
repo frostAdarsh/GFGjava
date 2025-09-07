a5311a4afb1c9ad78b0a1b59115a1db13e987aa0
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2025-09-07 22:26:29 +0530

    one

---
+++ src/myPractice/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,17 @@
+package myPractice;
+
+public class RemoveDuplicatesfromSortedArray {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int j = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[j] != nums[i]) {
+                    nums[++j] = nums[i];
+                }
+
+            }
+            return ++j;
+        }
+
+    }
+}
