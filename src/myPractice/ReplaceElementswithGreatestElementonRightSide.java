2c10d12f3f009d88cedc0cf97986cec9c56aa106
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2025-09-11 23:10:08 +0530

    one

---
+++ src/myPractice/ReplaceElementswithGreatestElementonRightSide.java
@@ -0,0 +1,16 @@
+package myPractice;
+
+public class ReplaceElementswithGreatestElementonRightSide {
+
+
+        public int[] replaceElements(int[] arr) {
+            int max = -1;
+            for (int i = arr.length - 1; i >= 0; i--) {
+                int temp = max;
+                max = Math.max(max, arr[i]);
+                arr[i] = temp;
+            }
+            return arr;
+        }
+
+}
