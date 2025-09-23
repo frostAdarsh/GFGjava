c43f3dad0f31b29acd4df8052bf78837a8433a01
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2025-09-23 08:34:43 +0530

    hashing one

---
+++ src/myPractice/LargestElementinArray.java
@@ -0,0 +1,16 @@
+package myPractice;
+
+public class LargestElementinArray {
+    class Solution {
+        public static int largest(int[] arr) {
+            int lar = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > lar) {
+                    lar = arr[i];
+                }
+            }
+            return lar;
+        }
+    }
+
+}
