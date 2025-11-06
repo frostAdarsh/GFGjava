823ee76cf0c66fba6fde316dd36c28a8f0384a00
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2025-11-06 23:16:50 +0530

    MajorityElement - 169

---
+++ src/myPractice/MajorityElement.java
@@ -0,0 +1,19 @@
+package myPractice;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        int majority = nums[0] ,votes =1;
+        for(int i=1;i<nums.length;i++){
+            if(votes == 0){
+                votes++;
+                majority = nums[i];
+            }else if(majority == nums[i]){
+                votes++;
+            }else
+                votes--;
+        }
+        return majority;
+    }
+}
