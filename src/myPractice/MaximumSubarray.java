8e1d7ee9837862b93d20272635573efd2dcf9093
Adarsh Mani <136229331+frostAdarsh@users.noreply.github.com>
2025-11-07 23:32:29 +0530

    MaximumSubarray - 53

---
+++ src/myPractice/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package myPractice;
+
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
